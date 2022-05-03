96360470215d1d458b6dac55fd78157a1124277a
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-05-04 00:39:13 +0100

    idk what i messed up but revert to earlier version of this file

--- routes/api/v1/allPlayers.js
+++ routes/api/v1/allPlayers.js
@@ -4,50 +4,49 @@ const express = require("express"),
   cache = require("memory-cache"),
   cors = require("cors");
 
-var timeout = 15000;
+  var timeout = 15000
 
-const mergeById = (a1, a2) =>
-  a1.map((itm) => ({
-    ...a2.find((item) => item.name === itm.name && item),
-    ...itm,
-  }));
-
-router.put("/", cors(), async function (req, res) {
-  if (req.header("AUTH_KEY") == process.env.AUTH_KEY) {
-    var allPlayers = await emc.getAllPlayers().then((players) => {
-        return players;
-      }),
-      players = req.body;
-
-    if (!allPlayers) return;
-    cache.put("players", mergeById(allPlayers, players));
-    res.status(200).json(mergeById(allPlayers, players));
-  } else res.status(401).send("PUT request unauthorized!");
-});
-
-router.get("/", async (req, res) => {
-
-    var allPlayers = await emc.getAllPlayers().then((players) => {
-      return players;
-    });
-
-    res.status(200).json(allPlayers).setTimeout(timeout);
-    cache.put("players", allPlayers);
+  const mergeByName = (a1, a2) => a1.map(itm => ({...a2.find((item) => (item.name === itm.name) && item), ...itm}))
   
-});
-
-router.get("/:playerName", async (req, res) => {
-  var cachedPlayers = cache.get("players"),
-    playerName = req.params.playerName.toLowerCase();
-
-  if (cachedPlayers) {
-    var player = cachedPlayers.find((p) => p.name.toLowerCase() == playerName);
-
-    if (!player) res.status(404).json("That player does not exist!");
-    else res.status(200).json(player).setTimeout(timeout);
-  } else {
-    res.status(202).json("Players have not been cached yet.");
+  router.put('/', cors(), async function (req, res) 
+  {
+      if (req.header('AUTH_KEY') == process.env.AUTH_KEY) {
+          var allPlayers = await emc.Nova.getAllPlayers().then(players => { return players }),
+              players = req.body
+  
+          if (!allPlayers) return sendError(res)
+  
+          var output = mergeByName(allPlayers, players)
+  
+          cache.put('players', output)
+          res.status(200).json(output).setTimeout(timeout)
+      }
+      else res.status(401).send("PUT request unauthorized!")
+  })
+  
+  router.get("/", async (req, res) => 
+  {
+      var cachedPlayers = cache.get('players')
+      if (cachedPlayers) res.status(200).json(cachedPlayers)
+      else res.status(204).json("Players have not been cached yet.")
+  })
+  
+  router.get("/:playerName", async (req, res) => 
+  {
+      var cachedPlayers = cache.get('players'),
+          playerName = req.params.playerName.toLowerCase()
+  
+      if (cachedPlayers) {
+          var player = cachedPlayers.find(p => p.name.toLowerCase() == playerName)
+  
+          if (!player) res.status(404).json("That player does not exist!")
+          else res.status(200).json(player).setTimeout(timeout)
+      } 
+      else res.status(204).json("Players have not been cached yet.")
+  })
+  
+  function sendError(res) {
+      res.status(500).json("An error occured fetching data, please try again.")
   }
-});
-
-module.exports = router;
+  
+  module.exports = router
